9fdc21d1767dfc1ecd66cea2897701ce9489cfa6
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-17 11:29:53 +0530

    [ADDED]:Dipak-Addition of Two Numbers

---
+++ Input.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Input{
+		public static void main(String args[]){
+				Scanner sc=new Scanner(System.in);
+				System.out.println("Enter The Two Values:-");
+				int a=sc.nextInt();
+				int b=sc.nextInt();
+				int c=a+b;
+				System.out.println("Addition:-"+c);
+			}
+	}
