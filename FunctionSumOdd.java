da3273363ca9aee0432f249630133b439ae3f859
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-19 13:40:55 +0530

    [ADDED]:Dipak - This Program Is Function Sum Odd in Class

---
+++ FunctionSumOdd.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+class FunctionSumOdd{
+	public static void Printsum(int n){
+		int sum=0;
+		for(int i=1;i<=n;i++){
+				if(i%2!=0){
+						sum=sum+i;
+					}
+			}
+			System.out.println(sum);
+	}
+
+	public static void main(String args[]){
+			Scanner sc=new Scanner(System.in);
+			int n=sc.nextInt();
+			Printsum(n);
+		}
+}
