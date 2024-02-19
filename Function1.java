1f0d4f855f6fd5a3c6fedfc56ed381e6242cab24
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-19 14:03:30 +0530

    [ADDED]:Dipak - Demonstrate Function The CalculateSum Of Two Numbers

---
+++ Function1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Function1{
+	public static int CalculateSum(int a,int b){
+			  int sum=a+b;
+			  return sum;
+		}
+	public static void main(String args[]){
+		Scanner sc =new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+
+		int sum=CalculateSum(a,b);
+		System.out.println("Sum of 2 Numbers is:-"+sum);
+	}
+}
