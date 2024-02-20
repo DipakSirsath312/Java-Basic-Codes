8e99be597d24b1b88c81cdd11234401b2a4c6e6a
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-20 18:13:35 +0530

    [ADDED]:Dipak - This Program Check a Value Provide Even&Odd Number

---
+++ Even_Odd.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Even_Odd{
+	public static void main(String args[]){
+			Scanner sc=new Scanner(System.in);
+			System.out.print("Enter Any Number:-");
+		 	 int n=sc.nextInt();
+		 	 if(n%2==0){
+				 	System.out.println("Number Is Even");
+				 }else{
+					 System.out.println("Number Is Odd");
+					 }
+		 }
+}
