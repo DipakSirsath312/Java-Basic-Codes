7c00bfd81cf65584ddee3a9a973a2adc2047ef00
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-21 18:43:20 +0530

    [ADDED]:Dipak - Demontrate Program Provide The AverageFunction

---
+++ AverageFunction.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class AverageFunction{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+
+		int Average=(a+b+c)/3;
+		System.out.println("Average Number is:-"+Average);
+	}
+}
