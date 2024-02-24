93682f01f4e1f489a748a371badbf7cc1b46045b
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-24 14:38:04 +0530

    [ADDED]:Dipak - Demonstrate Program Concatenation Of String

---
+++ Concatenation.java
@@ -0,0 +1,9 @@
+class Concatenation{
+	public static void main(String args[]){
+		String Firstname="Dipak";
+		String Lastname="Sirsath";
+		String Fullname=Firstname+Lastname;
+
+		System.out.println(Fullname);
+	}
+}
