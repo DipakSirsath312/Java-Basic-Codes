e1d87e569549e44bb3ebc9ba3603789725851ef4
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-24 14:31:08 +0530

    [ADDED]:Dipak - Demonstrate Program Will Be Concatenation Of Two String

---
+++ Concatenation_String.java
@@ -0,0 +1,9 @@
+class Concatenation_String{
+	public static void main(String args[]){
+		String a="Uchiha";
+		String b="Itachi";
+
+		System.out.println(a.concat(b));
+		System.out.println(b.concat(a));
+	}
+}
