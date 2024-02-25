514fc2ac9d66e5e340f3f2d08e12aacfab6b0af9
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-25 19:56:42 +0530

    [ADDED]:Dipak - Demonstrate Program SubString Function

---
+++ SubString.java
@@ -0,0 +1,9 @@
+class SubString{
+	public static void main(String args[]){
+		String Sentence="My Name Is Dipak Sirsath";
+		String First=Sentence.substring(0,Sentence.length());
+		String Name=Sentence.substring(11,Sentence.length());
+		System.out.println(First);
+		System.out.println(Name);
+	}
+}
