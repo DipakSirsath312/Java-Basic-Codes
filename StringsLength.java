a21644472f3202ed9e2efce00f78f11d82845520
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-26 18:56:12 +0530

    [ADDED]:Dipak - Demonstrate program Length Function In String

---
+++ StringsLength.java
@@ -0,0 +1,10 @@
+class StringsLength{
+	public static void main(String args[]){
+		String Firstname="Tony";
+		String Lastname="Stark";
+		String Fullname =Firstname+Lastname;
+
+		System.out.println(Fullname);
+		System.out.println(Fullname.length());
+	}
+}
