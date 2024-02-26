c980764e7851f590af988e2b612be391b96d8504
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-26 19:07:08 +0530

    [ADDED]:Diapk - Demonstrate Program Compare Function in String

---
+++ StringCompare.java
@@ -0,0 +1,12 @@
+class StringCompare{
+	public static void main(String args[]){
+		String Name1="Tony";
+		String Name2="Stark";
+		//compare
+		if(Name1.compareTo(Name2)==0){
+			System.out.println("String Are Equal");
+		}else{
+			System.out.println("String Are Not Equal");
+		}
+	}
+}
