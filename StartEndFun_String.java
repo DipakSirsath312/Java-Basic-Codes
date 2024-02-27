5745edd402bd6fb95c4bcac793afcd4d0a0cad47
DipakSirsath312 <dipaksirsath312@gmail.com>
2024-02-27 12:37:42 +0530

    [ADDED]:Dipak - Demonstrate Program Star_End Function in String

---
+++ StartEndFun_String.java
@@ -0,0 +1,8 @@
+class StartEndFun_String{
+	public static void main(String args[]){
+		String Name = "DipakSirsath";
+
+		System.out.println(Name.startsWith("Di"));
+		System.out.println(Name.endsWith("th"));
+	}
+}
